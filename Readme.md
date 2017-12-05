70d47897eabdbc6e8c8a35dcb4dd0b716dc3a029
nikhil sulegaon <nikhilsulegaon@gmail.com>
2017-12-05 13:14:03 -0700

    Utility script to create a pickle file of the dataset

--- Readme.md
+++ Readme.md
@@ -1,2 +1,3 @@
-#Object Location Prediction
+# Object Location Prediction
+
 Predict the x,y,z coordinates of an object using Neural Nets.
---
+++ utilities/create_pickle_of_data_set.py
@@ -0,0 +1,45 @@
+import gzip
+
+import numpy as np
+from PIL import Image
+import six.moves.cPickle as pickle
+import os
+
+from sklearn.model_selection import train_test_split
+
+path_to_images = '../utilities/images/'
+
+
+def extract_data(images):
+    x = []
+    y = []
+    for image_name in images:
+        path = path_to_images + image_name
+        np_array_of_image = np.asarray(Image.open(path).convert('L'), dtype='float64') / 255
+        x.append(np_array_of_image)
+        label = [float(c) for c in image_name[:image_name.index('.jpg')].split(',')]
+        y.append(np.asarray(label))
+    return x, y
+
+
+all_images = os.listdir(path_to_images)
+print('Extracting images from ' + path_to_images)
+train_data_images, test_data_images = train_test_split(all_images, test_size=0.1, random_state=42)
+
+train_x, train_y = extract_data(train_data_images)
+test_x, test_y = extract_data(test_data_images)
+
+data = dict()
+data['train'] = {}
+data['train']['data'] = np.array(train_x)
+data['train']['label'] = np.array(train_y)
+data['test'] = {}
+data['test']['data'] = np.array(test_x)
+data['test']['label'] = np.array(test_y)
+
+print("Dump to pumpkin.pkl.gz...")
+gzip_file = gzip.open('../data/pumpkin.pkl.gz', 'wb')
+pickle.dump(data, gzip_file)
+gzip_file.close()
+
+print('done')

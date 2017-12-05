86e9ac8f44838ae01a36de529436e233d99b5efd
nikhil sulegaon <nikhilsulegaon@gmail.com>
2017-12-05 16:15:46 -0700

    Renaming file

--- src/main.py
+++ src/main.py
@@ -1,12 +1,20 @@
-import os.path
-import random
+import gzip
+import pickle
 
-import numpy as np
-import scipy
 from keras.layers import Conv2D
 from keras.layers import Dense
+from keras.layers import Dropout
 from keras.layers import Flatten
+from keras.layers import MaxPool2D
 from keras.models import Sequential
+
+
+class DataSet:
+    def __init__(self, location):
+        with gzip.open(location, 'rb') as f:
+            data_set_dict = pickle.load(f)
+        self.train_x, self.train_y = data_set_dict['train']['data'], data_set_dict['train']['label']
+        self.test_x, self.test_y = data_set_dict['test']['data'], data_set_dict['test']['label']
 
 
 class CNN:
@@ -14,31 +22,42 @@ class CNN:
     CNN classifier
     """
 
-    def __init__(self, x, y, te_x, te_y, epochs=5, batch_size=128):
+    def __init__(self, train_x, train_y, test_x, test_y, epochs=75, batch_size=128):
         """
         initialize CNN classifier
         """
         self.batch_size = batch_size
         self.epochs = epochs
-        self.train_x = x
-        self.test_x = te_x
-        self.train_y = y
-        self.test_y = te_y
+        self.train_x = train_x
+        self.test_x = test_x
+        self.train_y = train_y
+        self.test_y = test_y
 
         # build  CNN model
 
         self.model = Sequential()
+        # self.model.add(MaxPool2D(pool_size=(2, 2),input_shape=(150,150,3)))
+        self.model.add(Conv2D(32, (3, 3), input_shape=(150, 150, 3)))
+        self.model.add(Conv2D(32, (3, 3)))
+        self.model.add(MaxPool2D(pool_size=(2, 2)))
 
-        self.model.add(Conv2D(20, (5, 5), input_shape=(150, 150, 3)))
-        self.model.add(Conv2D(20, (5, 5)))
-        self.model.add(Conv2D(20, (5, 5)))
-        self.model.add(Conv2D(20, (5, 5)))
+        self.model.add(Dropout(0.25))
 
-        # self.model.add(Dropout(0.25))
+        self.model.add(Conv2D(16, (3, 3)))
+        self.model.add(Conv2D(16, (3, 3)))
+        self.model.add(MaxPool2D(pool_size=(2, 2)))
+
+        # self.model.add(Conv2D(16, (3, 3)))
+        # self.model.add(Conv2D(16, (3, 3)))
+
+        self.model.add(Conv2D(8, (3, 3)))
+        self.model.add(Conv2D(8, (3, 3)))
+
+        # self.model.add(Conv2D(1, (3, 3)))
+
         self.model.add(Flatten())
-        # self.model.add(Dense(64))
-        self.model.add(Dense(9))
-        self.model.add(Dense(2))
+        # self.model.add(Dense(25))
+        self.model.add(Dense(3))
 
         self.model.compile(loss='mean_squared_error', optimizer='adam', metrics=['accuracy'])
 
@@ -46,48 +65,46 @@ class CNN:
         self.model.fit(self.train_x, self.train_y, self.batch_size, self.epochs)
 
     def evaluate(self):
+
+        """
+        test CNN classifier and get accuracy
+        :return: accuracy
+        """
         predicted = self.model.predict(self.test_x)
-
-        for index, prediction in enumerate(predicted):
-            print(self.test_y[index])
+        j = 0
+        count = 0
+        count_x = 0
+        count_y = 0
+        count_z = 0
+        for i in predicted:
+            print(self.test_y[j])
             print('predicted')
-            print(prediction)
+            print(i)
+            if ((abs(self.test_y[j][0] - i[0]) < 15) and (abs(self.test_y[j][1] - i[1]) < 15) and (
+                    abs(self.test_y[j][2] - i[2]) < 15)):
+                count = count + 1
 
+            if abs(self.test_y[j][0] - i[0]) < 15:
+                count_x = count_x + 1
+            if abs(self.test_y[j][1] - i[1]) < 15:
+                count_y = count_y + 1
+            if abs(self.test_y[j][2] - i[2]) < 15:
+                count_z = count_z + 1
+            j = j + 1
+        print(count)
+        print(count_x)
+        print(count_y)
+        print(count_z)
+        print(len(predicted))
         return self.model.evaluate(self.test_x, self.test_y, verbose=10)
 
 
 if __name__ == '__main__':
+    data_set = DataSet('../data/pumpkin.pkl.gz')
+    print(data_set.train_x.shape)
+    print(data_set.train_y.shape)
 
-    train_x = []
-    train_y = []
-    path = "/Users/koushikreddy/Downloads/image"
-
-    list_dir = [i[:-4] for i in os.listdir(path)]
-    list_dir.sort()
-
-    for f in list_dir:
-        val = []
-        for j in f.split(","):
-            val.append(j)
-
-        train_y.append(val[0])
-        train_y.append(val[2])
-        f = str(f) + '.jpg'
-        image = scipy.misc.imread(os.path.join(path, f))
-        train_x.append(image)
-
-    random.shuffle(train_x)
-
-    train_x = np.array(train_x)
-    print(train_x.shape)
-    print(train_x[0][0][0][0])
-    train_x = train_x / 255
-    print(train_x[0][0][0][0])
-    print(len(train_x))
-    train_y = np.array(train_y).reshape(len(train_x), 2)
-    print(train_y.shape)
-
-    cnn = CNN(train_x[:5000], train_y[:5000], train_x[:5001], train_y[:5001])
+    cnn = CNN(data_set.train_x, data_set.train_y, data_set.test_x, data_set.test_y)
     cnn.train()
     acc = cnn.evaluate()
     print(acc)

5e77c1b6084e8dd5cc084daf18b5da8b102a5be8
nikhil sulegaon <nikhilsulegaon@gmail.com>
2017-12-06 19:34:30 -0700

    Shuffling data before trianing the CNN

--- src/data_set_py2.py
+++ src/data_set_py2.py
@@ -1,6 +1,7 @@
 import gzip
 import pickle
 import numpy as np
+from sklearn.utils import shuffle
 
 
 class DataSet:
@@ -9,10 +10,12 @@ class DataSet:
             data_set_dict1 = pickle.load(f)
         with gzip.open(location2, 'rb') as f:
             data_set_dict2 = pickle.load(f)
-        self.train_x, self.train_y = np.append(data_set_dict1['train']['data'],
-                                               data_set_dict2['train']['data'], axis=0), \
-                                     np.append(data_set_dict1['train']['label'],
-                                               data_set_dict2['train']['label'], axis=0)
+        self.train_x, self.train_y = shuffle(np.append(data_set_dict1['train']['data'],
+                                                       data_set_dict2['train']['data'], axis=0),
+                                             np.append(data_set_dict1['train']['label'],
+                                                       data_set_dict2['train']['label'], axis=0))
 
-        self.test_x, self.test_y = np.append(data_set_dict1['test']['data'], data_set_dict2['test']['data'], axis=0),\
-                                   np.append(data_set_dict1['test']['label'], data_set_dict2['test']['label'], axis=0)
+        self.test_x, self.test_y = shuffle(np.append(data_set_dict1['test']['data'],
+                                                     data_set_dict2['test']['data'], axis=0),
+                                           np.append(data_set_dict1['test']['label'],
+                                                     data_set_dict2['test']['label'], axis=0))
--- src/data_set_py3.py
+++ src/data_set_py3.py
@@ -1,10 +1,11 @@
 import gzip
 import pickle
+from sklearn.utils import shuffle
 
 
 class DataSet:
     def __init__(self, location='../data/pumpkin.pkl.gz'):
         with gzip.open(location, 'rb') as f:
             data_set_dict = pickle.load(f)
-        self.train_x, self.train_y = data_set_dict['train']['data'], data_set_dict['train']['label']
-        self.test_x, self.test_y = data_set_dict['test']['data'], data_set_dict['test']['label']
+        self.train_x, self.train_y = shuffle(data_set_dict['train']['data'], data_set_dict['train']['label'])
+        self.test_x, self.test_y = shuffle(data_set_dict['test']['data'], data_set_dict['test']['label'])
